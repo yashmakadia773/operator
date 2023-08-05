8a62aabee4d79279e08819cfae9332f4a0eac1a2
yashmakadia773 <133627880+yashmakadia773@users.noreply.github.com>
2023-08-05 14:22:33 +0530

    Add files via upload

---
+++ if-statment.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	int a;
+	printf("Enter value of A :- ");
+	scanf("%d",&a);
+	
+	if(a<0)
+	{
+		printf("number is not nutral");	
+		
+	}	
+	else if(a>0)
+	{
+		printf("number is not nutral");
+	}
+	else
+	{
+		printf("number is nutral");	
+	}
+}
---
+++ if-statment2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+main()
+{
+	int a,b;
+	printf("Enter value of A :- ");
+	scanf("%d",&a);
+	printf("Enter value of B :- ");
+	scanf("%d",&b);
+	
+	if(a<b)
+	{
+	
+		printf("%d is minimum",a);	
+		
+	}	
+	else
+	{
+		printf("%d is minimum",b);
+	}
+}
